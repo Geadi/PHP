db1416c81081d94e190ff46aa90980723da6e72a
Geadi <66213778+Geadi@users.noreply.github.com>
2020-07-14 19:46:12 +0300

    add

---
+++ part3-Constants/index.php
@@ -0,0 +1,9 @@
+<?php
+    echo PHP_VERSION; //ВЕРСИЯ PHP
+
+    define("PI", 3.14); // СОЗДАЕТ КОНСТАНТНУЮ ПЕРЕМЕННУЮ
+    echo "<br/>";
+    echo PI;
+    echo "<br/>";
+    echo defined("PI"); // ИЩЕТ ЗАДАННУЮ КОНСТАНТУ И ВЫВОДИТ true
+?>
