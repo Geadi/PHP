e2961267c469d773e5ac71cfefdcce8f2e577a04
Geadi <66213778+Geadi@users.noreply.github.com>
2020-07-14 19:39:20 +0300

    add

---
+++ part2-Variables and Data Types/index.php
@@ -0,0 +1,17 @@
+<?php
+    $number = 45;
+    $float = -3.5;
+    $string = "Hello World!";
+    $bold = true;
+
+    echo "Переменная number = $number";
+    echo "<br/>";
+    echo "Переменная float = $float";
+    echo "<br/>";
+    echo "Переменная string = $string";
+    echo "<br/>";
+    echo "Переменная bold = $bold";
+    echo "<br/>";
+    $number = 12;
+    echo "Переменная number = $number";
+?>
