add8e900824271df5cae30b39bf57347316827f3
Geadi <66213778+Geadi@users.noreply.github.com>
2020-07-16 14:53:10 +0300

    add

--- part15-File Connection/index.php
+++ part15-File Connection/index.php
@@ -1,5 +1,28 @@
 <?php
 
+    /*
+    require - загружает файлы используя указанный путь, если в пути 
+    файла будит ошибка, то приостановит работу программы.
+
+    include - делает тоже самое что и require, но если в пути файла 
+    будит ошибка, то он не приостановит работу программы.
+
+    include_once - файл подключиться один раз
+    require_once - файл подключиться один раз
+    */ 
+
+
+    /*
+    include "header.php";
+    include "footer.php";
+
+    include_once "header.php";
+    include_once "footer.php";
+
+    require_once "header.php";
+    require_once "footer.php";
+    */
+
     $title = "Заголовок страницы";
     require "header.php";
     echo "Тело документа";
