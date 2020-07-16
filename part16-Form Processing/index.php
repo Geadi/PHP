3ad76ba64de38e948f489d9273dd2fff69d1f964
Geadi <66213778+Geadi@users.noreply.github.com>
2020-07-16 16:24:57 +0300

    add

---
+++ part16-Form Processing/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form name="test" action="check.php" method="post">
+        <label>Имя: </label><br/>
+        <input type="text" name="name" placeholder="Имя">
+        <label>Email: </label><br/>
+        <input type="text" name="email" placeholder="Email">
+        <label>Сообщение: </label><br/>
+        <textarea name="message" cols="40" rows="10"></textarea><br/>
+        <input type="submit" name="done" velue="Готов">
+    </form>
+</body>
+</html>
